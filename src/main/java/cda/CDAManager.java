788de6980e94105fc2acd50a4cbde7134cb6a5be
Nick Zárate <nickzaro@gmail.com>
2022-05-24 12:03:20 -0500

    Agregando fetchWithContentType, busqueda por Contentype

--- src/main/java/cda/CDAManager.java
+++ src/main/java/cda/CDAManager.java
@@ -3,7 +3,9 @@ package cda;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import com.contentful.java.cda.CDAArray;
 import com.contentful.java.cda.CDAClient;
 import com.contentful.java.cda.CDAEntry;
 
@@ -11,21 +13,23 @@ import cda.custom.CDACustom;
 import cda.parser.CDAParser;
 
 public final class CDAManager {
-	private final static String space ="k9b41bs3k4e1";
-	private final static String token= "qOdE1hZZpxgpRJRSE2uAUKlgUpvc0p0hJpd3Q-qghCk";
+	private final static String space = "k9b41bs3k4e1";
+	private final static String token = "qOdE1hZZpxgpRJRSE2uAUKlgUpvc0p0hJpd3Q-qghCk";
 	private final static CDAClient client = CDAClient.builder().setSpace(space).setToken(token).build();
-	private final static CDAManager cdaManager= new CDAManager();
-	private CDAManager() {		
+	private final static CDAManager cdaManager = new CDAManager();
+
+	private CDAManager() {
 	}
 
 	public static CDAManager getInstance() {
 		return cdaManager;
 	}
-	
+
 	public static String fetch(String entry) {
 		CDAEntry cdaEntry = client.fetch(CDAEntry.class).one(entry);
 		CDAParser parser = CDAParser.getInstance();
 		ArrayList<CDACustom> cdas = new ArrayList<CDACustom>();
+		// transformar a fecha de formato del wcm_portal 
 		cdas.add(parser.evaluar("createdAt", cdaEntry.attrs().get("createdAt")));
 		cdas.add(parser.evaluar("updatedAt", cdaEntry.attrs().get("updatedAt")));
 
@@ -33,11 +37,13 @@ public final class CDAManager {
 
 			CDACustom cda;
 			if ((cda = parser.evaluar(ent.getKey(), cdaEntry.getField(ent.getKey()))) != null) {
+				System.out.println("TRUE=== " + cdaEntry.getField(ent.getKey()).getClass() + "--- " + ent.getKey()
+				+ " -> " + cdaEntry.getField(ent.getKey()).toString());
 				cdas.add(cda);
 			} else {
 				System.out.println("FALSE=== " + cdaEntry.getField(ent.getKey()).getClass() + "--- " + ent.getKey()
 						+ " -> " + cdaEntry.getField(ent.getKey()).toString());
-				//TODO: una exception debe lanzarse
+				// TODO: una exception debe lanzarse
 			}
 
 		}
@@ -45,7 +51,28 @@ public final class CDAManager {
 		cdas.forEach((cda) -> hashs.putAll(cda.getHashMap()));
 		return hashs.toString();
 
-		
-		
+	}
+	
+	public static String fetchWithContentType(String contentTypeId) {
+		CDAArray cdaArray = client.fetch(CDAEntry.class).
+				withContentType(contentTypeId).all();
+		System.out.println(cdaArray);
+		ArrayList<CDACustom> cdas = new ArrayList<CDACustom>();
+		CDAParser parser = CDAParser.getInstance();
+		for (Entry<String, CDAEntry> ent : cdaArray.entries().entrySet()) {
+
+			CDACustom cda;
+			if ((cda = parser.evaluar(ent.getKey(), ent.getValue())) != null) {
+				System.out.println("TRUE=== " + ent.getValue().getClass() + "--- " + ent.getKey()
+				+ " -> " + ent.toString());
+				cdas.add(cda);
+			} else {
+				System.out.println("FALSE=== " + ent.getValue().getClass() + "--- " + ent.getKey()
+				+ " -> " + ent.toString());
+				// TODO: una exception debe lanzarse
+			}
+
+		}
+		return cdas.toString();
 	}
 }
--- src/main/java/cda/ContentfulTest.java
+++ src/main/java/cda/ContentfulTest.java
@@ -6,7 +6,18 @@ public class ContentfulTest {
 
 	public static void main(String[] args)
 			throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-		System.out.println(CDAManager.fetch("7i1r6q6MxFdH2V7Rp191gH"));
+		//System.out.println(CDAManager.fetch("7i1r6q6MxFdH2V7Rp191gH"));
+		//System.out.println(CDAManager.fetch("4Lqk4bZp7sRkZIoGD1Gejy"));
+		
+		//System.out.println(CDAManager.fetch("6Iq6Oks2cEjEx5W01FLGbh"));
+		//System.out.println(CDAManager.fetch("3mch2jCVanupuGeR4cKB0V"));
+		//System.out.println(CDAManager.fetch("2EPWCBO8HeMdY5tRDYJxkI"));
+		
+		// System.out.println(CDAManager.fetch("3PeLjc7ZcnQTqPWghwjPKM"));
+		System.out.println(CDAManager.fetchWithContentType("Footer"));
+		
+		
+		
 
 	}
 
